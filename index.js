4a2016da7c60763bbb6a9dd2b7815391a881d765
pbriseno <pbj_13_130910@hotmail.es>
2022-02-20 17:43:39 -0600

    Fix hyperlink to source CSS and JS files, formatted HTML

--- index.js
+++ index.js
@@ -47,23 +47,14 @@ function addZero(min) {
 let currentMinute = addZero(currentDate.getMinutes());
 
 let h3 = document.querySelector("h3");
-h3.innerHTML = ` ${currentDay} ${currentDt} ${currentMonth} ${currentYear} ${currentHour}:${currentMinute}`;
+h3.innerHTML = `${currentDay} ${currentDt} ${currentMonth} ${currentYear} ${currentHour}:${currentMinute}`;
 
 //Function to prevent default behavior
 function preDefault(event) {
   event.preventDefault();
 }
 
-function showTemperature(response) {
-  let temperature = Math.round(response.data.main.temp);
-  let head = document.querySelector("#temperature");
-  head.innerHTML = `${temperature}°C`;
-}
-
 //Function to show current city with geocoding
-
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", handleSubmit);
 
 function searchCity(city) {
   let apiKey = "abd208cd7d5195517375f60fa893a1cc";
@@ -82,6 +73,7 @@ function searchLocation(position) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayWeatherCondition);
+  //axios.get(apiUrl).then(showTemperature);
 }
 
 function getCurrentLocation(event) {
@@ -89,9 +81,27 @@ function getCurrentLocation(event) {
   navigator.geolocation.getCurrentPosition(searchLocation);
 }
 
+function displayWeatherCondition(response) {
+  document.querySelector("#location").innerHTML = response.data.name;
+  //document.querySelector("#temperature").innerHTML = response.data.main.temp;
+  let temperature = Math.round(response.data.main.temp);
+  let head = document.querySelector("#temperature");
+  head.innerHTML = `${temperature}°C`;
+  document.querySelector("#weather").innerHTML =
+    response.data.weather[0].description;
+
+  //console.log(response);
+}
+
+/*function showTemperature(response) {
+  let temperature = Math.round(response.data.main.temp);
+  let head = document.querySelector("#temperature");
+  head.innerHTML = `${temperature}°C`;
+  console.log(temperature);
+}*/
+
 let button = document.querySelector("#current");
 button.addEventListener("click", getCurrentLocation);
 
-function displayWeatherCondition(response) {
-  document.querySelector("#location").innerHTML = response.data.name;
-}
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
